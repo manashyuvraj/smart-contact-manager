d48e7719c631b154d6e9ba259cec98d617476f7d
Manash Yuvraj Sharma <manashyuvraj@gmail.com>
2026-02-14 16:24:22 +0530

    templates are added and inherited to the base template

---
+++ src/main/java/com/scm/smartcontactmanager/controllers/PageController.java
@@ -0,0 +1,28 @@
+package com.scm.smartcontactmanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/services")
+    public String services(){
+        return "services";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+}
